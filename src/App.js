855ac2b0a9adc37bd1cac3e6c2d1a4a7854711f6
SylvainJunca <sylvain.junca@gmail.com>
2019-01-13 18:59:58 -0500

    fetch an API now to get the list of users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 
@@ -11,11 +10,16 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
-
+  
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
+  }
   onSearchChange = (event) => {
     this.setState( { searchfield: event.target.value});
   }
@@ -28,7 +32,7 @@ class App extends Component {
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots= {filteredRobots} />
+        {this.state.robots ? <CardList robots= {filteredRobots} /> : 'Loading'}
       </div>
     )
   }   
