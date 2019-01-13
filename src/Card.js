95a923959ed9c5afc12bc2cb8022b0b21919ea81
SylvainJunca <sylvain.junca@gmail.com>
2019-01-13 14:43:24 -0500

    creates the Card component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img alt='avatar' src='https://robohash.org/JaneDoe?200x200' />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+export default Card;
