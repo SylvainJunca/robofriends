c3f72e97ad3794e3f28c12c11981071b4fcbd76d
SylvainJunca <sylvain.junca@gmail.com>
2019-01-13 16:36:37 -0500

    recreates the App component

--- src/CardList.js
+++ src/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
   const cardComponent = robots.map((user, i) => {
-    return <Card id={user.id} name={user.name} email={user.email} />
+    return <Card key={i} id={user.id} name={user.name} email={user.email} />
   })
   return (
     <div>
