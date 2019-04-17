85d035197e36584d250d23b30c702e205f250aac
SylvainJunca <sylvain.junca@gmail.com>
2019-04-17 19:02:41 -0400

    Updates

---
+++ precache-manifest.55c783a79810249ce15e674e8bdf7051.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "f185e2549da28470bcdb",
+    "url": "/robofriends/static/js/main.87e39c4d.chunk.js"
+  },
+  {
+    "revision": "f9f88592014fc8789226",
+    "url": "/robofriends/static/js/2.53a1a8f5.chunk.js"
+  },
+  {
+    "revision": "f185e2549da28470bcdb",
+    "url": "/robofriends/static/css/main.db72ebb4.chunk.css"
+  },
+  {
+    "revision": "f9f88592014fc8789226",
+    "url": "/robofriends/static/css/2.ad34b7e5.chunk.css"
+  },
+  {
+    "revision": "6cca193a8ccfc33cba6e99323fef0ad7",
+    "url": "/robofriends/index.html"
+  }
+];
